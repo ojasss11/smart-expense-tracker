c30a36fa47840e232cfa1f83d2f079d7b0689098
anuradhakumari2025 <krianuradha4321@gmail.com>
2025-01-07 10:01:45 +0530

    Added files in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Route,Routes } from 'react-router-dom';
 import UserLogin from './pages/UserLogin';
 import UserSignup from './pages/UserSignup';
 import Home from './pages/Home';
+import AdminLogin from './pages/AdminLogin';
+import AdminSignup from './pages/AdminSignup';
 function App() {
 
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<UserLogin />} />
           <Route path="/signup" element={<UserSignup />} />
+          <Route path='/adminlogin' element={<AdminLogin/>}/>
+          <Route path='/adminsignup' element={<AdminSignup/>}/>
         </Routes>
         </div>     
     </>
